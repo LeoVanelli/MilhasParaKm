dc1fac6a77141f552489874f90cf6f60a6d2072c
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-09-26 23:34:23 -0300

    Adiciona atividade MilhasParaKm

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Objetivo: Receba uma medida em milhas e exiba seu equivalente em quilômetros. A medida em km é 1,609 vezes a medida em milhas.
+
+            Console.Write("Entre com a medida (em milhas): ");
+
+            double medida = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Milhas: {medida}\nQuilômetros: {medida*1609}");
+        }
+    }
+}
